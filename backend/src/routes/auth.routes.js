3bacbb2a79e790f48b28e8ad9b3bd5f09ec91f3a
gordo240620 <manuelpacheco031105@gmail.com>
2026-03-13 21:19:21 -0600

    perfil conectado con backend y breadcrum

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { register, login } from '../controllers/auth.controller.js';
+import { register, login, updateProfile } from '../controllers/auth.controller.js';
+import { authenticateToken } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.put('/update', authenticateToken, updateProfile);
 
 export default router;
